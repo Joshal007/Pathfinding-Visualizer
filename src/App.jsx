23a427fbe4046c8d390f4015b3f95e67f95e644c
Joshal007 <joshaljoshi07@gmail.com>
2023-10-06 23:41:48 +0530

    setup gh-pages

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Grid from './components/Grid/Grid'
+import Navbar from './components/Navbar/Navbar'
+
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+     <Navbar></Navbar>
+     <Grid></Grid>
+     <h4>Made By Joshal @2023</h4>
+    </div>
+  )
+}
+
+export default App
